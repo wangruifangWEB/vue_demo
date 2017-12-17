e4763d91d8fe4656b98cc2d1a410ee592a304df8
wangruifang <wangruifang@buqiuren.com.cn>
2017-12-17 17:00:51 +0800

    组件各种组合使用

---
+++ js/tpl/login.js
@@ -0,0 +1,3 @@
+const login = Vue.component('login-component', {
+    template: '<h1> Hello Login</h1>'
+})
---
+++ js/tpl/main.js
@@ -0,0 +1,3 @@
+const main = Vue.component('main-component', {
+    template: '<h1> Hello Main</h1>'
+})
